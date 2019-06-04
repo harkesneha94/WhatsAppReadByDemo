aea48718e4430780bb98a03ada28dadcd07f09a2
Sneha Harke <sneha.harke@ril.com>
2019-06-04 21:20:20 +0530

    Initial Commit

---
+++ WhatsAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsAppDemo
+//
+//  Created by Sneha Harke on 04/06/19.
+//  Copyright © 2019 Sneha Harke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
